5c1df46a8668c109cd8822aa3c8bfd0de2f63479
Brad Hilton <brad.hilton.nw@gmail.com>
2025-08-05 22:47:36 +0000

    feat: Add GMPO support

--- src/art/unsloth/train.py
+++ src/art/unsloth/train.py
@@ -85,9 +85,9 @@ def get_compute_loss_fn(trainer: "GRPOTrainer") -> Callable[..., torch.Tensor]:
         next_input_ids = shift_tensor(inputs["tokens"], 0)
         chunk_size = _config.get("logprob_calculation_chunk_size", 1024)
         # Assert that sequence length is evenly divisible by the chunk size
-        assert seq_len % chunk_size == 0, (
-            f"Sequence length ({seq_len}) must be evenly divisible by chunk size ({chunk_size})"
-        )
+        assert (
+            seq_len % chunk_size == 0
+        ), f"Sequence length ({seq_len}) must be evenly divisible by chunk size ({chunk_size})"
         os.environ["UNSLOTH_RETURN_HIDDEN_STATES"] = "1"
         new_logprobs, entropies = calculate_logprobs(
             autocast_dtype,
@@ -154,10 +154,23 @@ def get_compute_loss_fn(trainer: "GRPOTrainer") -> Callable[..., torch.Tensor]:
             prob_ratio = torch.clamp(
                 prob_ratio, max=max_negative_advantage_importance_sampling_weight
             )
-        policy_loss = -torch.min(
-            prob_ratio * advantages,
-            torch.clip(prob_ratio, 1 - epsilon, 1 + epsilon_high) * advantages,
-        )
+        if _config.get("gmpo", True):
+            advantage_signs = -torch.sign(advantages)
+            signed_logprob_diff = logprob_diff * advantage_signs
+            signed_logprob_diff_clamp = torch.clamp(
+                signed_logprob_diff, -epsilon, epsilon_high
+            )
+            signed_logprob_diff_max = torch.max(
+                signed_logprob_diff, signed_logprob_diff_clamp
+            )
+            logprobs_diff_max = advantage_signs * signed_logprob_diff_max
+            prob_ratio = torch.exp(logprob_diff)
+            policy_loss = -advantages * logprobs_diff_max
+        else:
+            policy_loss = -torch.min(
+                prob_ratio * advantages,
+                torch.clip(prob_ratio, 1 - epsilon, 1 + epsilon_high) * advantages,
+            )
         if ref_logprobs is not None:
             kl_div = (
                 torch.exp(ref_logprobs - new_logprobs)
@@ -323,7 +336,9 @@ def _calculate_logprobs(
         chunk_logits = torch.matmul(chunk_hs, lm_head_t)  # [B, chunk_size, V]
         chunk_selected_logits = torch.gather(
             chunk_logits, dim=-1, index=chunk_input_ids.unsqueeze(-1)
-        ).squeeze(-1)  # [B, chunk_size]
+        ).squeeze(
+            -1
+        )  # [B, chunk_size]
         chunk_logsumexp = torch.logsumexp(chunk_logits, dim=-1)  # [B, chunk_size]
         log_probs[:, i : i + chunk_size] = chunk_selected_logits - chunk_logsumexp
 
